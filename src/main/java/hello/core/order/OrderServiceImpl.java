89b175610eb5c1657220baf7c34a3df53fb8397c
430lyj <430lyj@gmail.com>
2022-01-21 03:36:03 +0900

    feat: 새로운 할인 정책(정률 할인 정책) 개발

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,7 +11,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 여기서는 DIP(의존성 역전 원칙) 위반 > 추상화는 세부 사항에 의존해서는 안된다. (여기서는 추상화인 인터페이스가 구체화된 클래스에 의존 중)
+    // + OCP도 위반. (클라이언트 코드인 서비스 코드의 변경 발생!)
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
