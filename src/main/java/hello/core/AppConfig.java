7aa520decb8623bf873f49d3856493d0c53886cb
430lyj <430lyj@gmail.com>
2022-01-23 00:01:34 +0900

    test: 스프링 싱글톤 컨테이너 test

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,21 +13,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //Application 전체를 설정하고 구성하는 파일 (공연 기획자와 같은 역할) > 이제 클라이언트 코드를 수정할 필요가 없음.
-@Configuration //Application의 설정정보를 담은 클래스다 라고 명시해주는 것.
+@Configuration //Application의 설정정보를 담은 클래스다 라고 명시해주는 것. (singleton을 위해 존재 -> 없으면 스프링 빈 등록은 되지만 싱글톤x)
 public class AppConfig {
     //refactoring : 각각이 하나의 역할만을 담당할 수 있도록 변환됨. (역할에 대한 구현이 드러남)
+
+    //@Bean memberService -> new MemoryMemberRepository
+    //@Bean orderService -> new MemoryMemberRepository, new RateDiscountPolicy -> 과연 싱글톤 패턴이 깨진 걸까?
     @Bean
     public MemberService memberService(){ //AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해 주입(연결) 해준다 고 표현
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
@@ -36,4 +42,5 @@ public class AppConfig {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
+
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -24,4 +24,9 @@ public class OrderServiceImpl implements OrderService {
         //단일 책임 원칙이 잘 지켜진 것. 할인 관련한 건 난 모르겠고, discountPolicy에서 해결해줘 와 같은 것
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
