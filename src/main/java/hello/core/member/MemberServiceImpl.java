531758a9d4db1278508ed716eca33f7e77822ba6
430lyj <430lyj@gmail.com>
2022-01-23 16:08:07 +0900

    refactor: 컴포넌트 스캔 방식으로 변환

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository; //추상화에만 의존하게 되었음 (No more MemoryMemberRepository!)
 
+    @Autowired //자동 의존관계 생성
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
