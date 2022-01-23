e1781ef6501472db36972aea1a328952cc0b6680
430lyj <430lyj@gmail.com>
2022-01-24 04:53:45 +0900

    refactor: Lombok 사용, @Qualifier, @Primary 사용

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@Component 애노테이션이 있는 클래스를 찾아서 다 자동으로 스프링 빈으로 등록해줌.
 @ComponentScan(
-        basePackages = "hello.core.member", //이 위치에서부터 하위 패키지로 찾아 들어감. 여러 개 둘 수도 있음.
+        // basePackages = "hello.core.member", //이 위치에서부터 하위 패키지로 찾아 들어감. 여러 개 둘 수도 있음.
         // 지정하지 않으면 해당 설정정보 클래스의 패키지가 시작위치
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
  ) //AppConfig는 수동으로 등록하는 거기 때문에 AppConfig를 제외해줌.
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,21 +1,25 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor //필수 필드인 final이 붙은 값들을 가지고 생성자를 만들어주는 annotation (@Autowired 붙은 생성사 불필요!)
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository; //필드 주입 -> 권장하지 않음. 순수 자바 코드로는 외부에서 변경이 불가능해서 setter를 또 만들어줘야함. (수정자로 돌아감)
     private final DiscountPolicy discountPolicy;
 
-    @Autowired //생성자가 하나인 경우는 @Autowired 생략해도 똑같은 효과 (자동 의존관계 주입)
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository,@MainDiscountPolicy DiscountPolicy discountPolicy) { //@Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    } //순수 인터페이스에만 의존. 남자 배우(OrderServiceImpl) 입장에서는 여자 배우가 존재한다는 것만 알고, 어떤 배우가 캐스팅되었는지는 모르는 것!
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
